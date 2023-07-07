e2aa25eb12b3b9bc13140ca60d6a849678ca1dac
khushi <khushiramrajliwal@gmail.com>
2023-07-07 17:10:44 +0530

    token delete after click on reset password link

--- Autntoken.js
+++ Autntoken.js
@@ -3,6 +3,7 @@ import { editUserById } from "./databaseQueries.js";
 
 
 export const secretkey="lkjhgfdsaqwertyuiopmnbvcxzlokiju"
+
 export async function generateToken(id){
     try{  
         let token= jwt.sign({_id:id},secretkey,{expiresIn:"1d"})
